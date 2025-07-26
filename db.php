23d27bf947b231857c37ed3c76724703cad1c9c3
bibaowen <bobby.owen@gmail.com>
2025-07-26 08:38:01 -0800

    Add files via upload

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost";
+$user = "autopjoo_practitioners";
+$pass = "Biba2@portmore";
+$db = "autopjoo_medical_ai";
+
+/*$host = "localhost";
+$user = "root";
+$pass = "owen";
+$db = "medical_ai";*/
+
+$conn = new mysqli($host, $user, $pass, $db);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
